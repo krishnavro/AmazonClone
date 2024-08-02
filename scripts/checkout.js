3be93cc8cae750878117f4129da8a4fa4950d360
krishnakumbhar422 <shrikrishnakumbhar267@gmail.com>
2024-08-02 18:02:44 +0530

    Convert from promises and async await

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -6,6 +6,30 @@ import { loadCart } from "../data/cart.js";
 // import '../data/cart-class.js';
 // import '../data/backend-practice.js';
 
+
+async function loadPage(){
+
+
+
+await loadProductsFetch();
+
+
+await new Promise((resolve) => {
+    loadCart(() => {
+        resolve();
+    });
+});
+
+renderOrderSummary();
+renderPaymentSummary();
+
+
+
+}
+loadPage();
+
+
+/*
 Promise.all([
    loadProductsFetch(),
        new Promise((resolve) => {
@@ -20,6 +44,7 @@ Promise.all([
     renderOrderSummary();
     renderPaymentSummary();
 });
+*/
 
 /*
 new Promise((resolve) => {
