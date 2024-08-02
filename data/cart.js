7846812388b1ad4b3413e7c6b334316aa721ac5c
krishnakumbhar422 <shrikrishnakumbhar267@gmail.com>
2024-08-02 15:21:52 +0530

    Learn romises and convert out code to promises

--- data/cart.js
+++ data/cart.js
@@ -76,3 +76,17 @@ function saveToStorage(){
 
     saveToStorage();
   }
+
+
+  export function loadCart(fun) {
+    const xhr = new XMLHttpRequest();
+    xhr.addEventListener('load', () => {
+      console.log(xhr.response); 
+    fun();
+    });
+   
+   
+   
+    xhr.open('GET', 'https://supersimplebackend.dev/cart');
+    xhr.send(); 
+   }
